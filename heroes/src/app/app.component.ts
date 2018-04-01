d085049b42b8e21988f6cc21fce05b16e925a4df
George Vrettos <lukedevgr@gmail.com>
2018-04-01 08:10:48 +0300

    Fix minor typos

--- heroes/src/app/app.component.ts
+++ heroes/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Java(Sript) coding shcool'
+  title = 'Java(Script) coding school'
 }
